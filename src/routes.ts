55eb614a6078a99b21236994cf0d8518ac4cce46
Eric William da Silva Marques <eric.marques@benner.com.br>
2021-06-22 22:27:19 -0300

    marques || 2206
    Aula 3: Realizando o tratamento de exceções e criando demais funcionalidade para as entidade TAG

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { request, response, Router } from "express";
 import { CreateUserController } from "./controller/User/CreateUserController";
+import { CreateTagController } from "./controller/Tag/CreateTagController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const createTafController = new CreateTagController();
 
 router.post("/users", createUserController.handle);
+router.post("/tags", ensureAdmin, createTafController.handle);
 
 export { router }
